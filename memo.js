905cacbbaa1d98e5e6040d6797aa7a34a4ba07a6
shirotamaki <shiro.tamaki63@gmail.com>
2021-12-08 22:33:58 +0900

    クラスの実装途中まで。Storageクラスは実装してみた。

--- memo.js
+++ memo.js
@@ -1,80 +1,80 @@
 const Enquirer = require('enquirer')
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./memo.db')
 
-// メモを追加用
-function main (stdin) {
-  db.run('CREATE TABLE if not exists foodb (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)')
-  db.run('INSERT INTO foodb (content) VALUES (?)', stdin)
-  db.close()
-}
+class Storage {
+  constructor () {
+    const sqlite3 = require('sqlite3').verbose()
+    this.db = new sqlite3.Database('./memo.db')
 
-// lオプション
-function displayLoption () {
-  db.each('SELECT * FROM foodb', (error, row) => {
-    if (error) {
-      console.error('Error!', error)
-      return
-    }
-    console.log(row.content.split('\n')[0])
-    console.log(row)
-  })
-  db.close()
-}
+    this.initialize()
+  }
 
-// dオプション
-function displayDoption () {
-  db.all('SELECT * FROM foodb', (error, rows) => {
-    if (error) {
-      console.error('Error!', error)
-      return
-    }
-    const memoTitles = rows.map(({ content }) => content.split('\n')[0])
-    const memos = rows
-    const values = {
-      type: 'select',
-      name: 'memoTitle',
-      message: 'Choose a note you want to see:',
-      choices: memoTitles,
-    }
-    const answer = Enquirer.prompt(values)
-    answer.then(({ memoTitle }) => {
-      const memoIds = memos.find(element => element.content.split('\n')[0] === memoTitle)
-      const memoId = memoIds.id
-      console.log(memoId)
-      db.run('DELETE FROM foodb WHERE id = ?', memoId, error => {
+  initialize () {
+    this.db.run('CREATE TABLE if not exists foodb (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)')
+  }
+
+  insert (stdin) {
+    this.db.run('INSERT INTO foodb (content) VALUES (?)', stdin)
+  }
+
+  getDbObj () {
+    return new Promise(function (resolve, reject) {
+      this.db.all('SELECT * FROM foodb', (error, rows) => {
         if (error) {
-          return console.error(error.message)
+          console.error('Error!', error)
+          return
         }
+        resolve(rows)
       })
-      db.close()
     })
+  }
+
+  selectMemo (rows) {
+    return new Promise(function (resolve, reject) {
+      const memosTitle = rows.map(({ content }) => content.split('\n')[0])
+      const values = {
+        type: 'select',
+        name: 'memoTitle',
+        message: 'Choose a note you want to see:',
+        choices: memosTitle,
+      }
+      const memo = Enquirer.prompt(values)
+      memo.then(({ memoTitle }) => {
+        const selectedMemo = rows.find(element => element.content.split('\n')[0] === memoTitle)
+        resolve(selectedMemo)
+      })
+    })
+  }
+
+  deleteMemo (memoId) {
+    this.db.run('DELETE FROM foodb WHERE id = ?', memoId, error => {
+      if (error) {
+        return console.error(error.message)
+      }
+    })
+  }
+}
+
+const storage = new Storage()
+// lオプション
+async function displayLoption () {
+  const rows = await storage.getDbObj()
+  rows.forEach(function (memoTitle) {
+    console.log(memoTitle.content.split('\n')[0])
   })
 }
 
 // rオプション
-function displayRoption () {
-  db.all('SELECT * FROM foodb', (error, rows) => {
-    if (error) {
-      console.error('Error!', error)
-      return
-    }
-    const memoTitles = rows.map(({ content }) => content.split('\n')[0])
-    const memoAllData = rows.map(({ content }) => content)
-    const values = {
-      type: 'select',
-      name: 'memoTitle',
-      message: 'Choose a note you want to see:',
-      choices: memoTitles,
-    }
-    const answer = Enquirer.prompt(values)
-    answer.then(({ memoTitle }) => {
-      const memoContent = memoAllData.find(element => element.split('\n')[0] === memoTitle)
-      console.log(memoTitle)
-      console.log(memoContent)
-    })
-  })
-  db.close()
+async function displayRoption () {
+  const rows = await storage.getDbObj()
+  const selectedMemo = await storage.selectMemo(rows)
+  console.log(selectedMemo.content)
+}
+
+// dオプション
+async function displayDoption () {
+  const rows = await storage.getDbObj()
+  const selectedMemo = await storage.selectMemo(rows)
+  await deleteMemo(selectedMemo.id)
 }
 
 // オプション受け付け
@@ -86,7 +86,8 @@ if (args == '-l') {
 } else if (args == '-d') {
   displayDoption()
 } else if (args == '') {
-  main(require('fs').readFileSync('/dev/stdin', 'utf8'))
+  const storage = new Storage()
+  storage.insert(require('fs').readFileSync('/dev/stdin', 'utf8'))
 } else {
   console.log('引数に誤りがあります')
 }
--- test.js
+++ test.js
@@ -86,3 +86,103 @@ if (args == '-l') {
 } else {
   console.log('引数に誤りがあります')
 }
+
+
+
+
+// promise, thenバージョン
+// promise, thenバージョン
+// promise, thenバージョン
+const Enquirer = require('enquirer')
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./memo.db')
+
+// メモを追加用
+function main (stdin) {
+  db.run('CREATE TABLE if not exists foodb (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)')
+  db.run('INSERT INTO foodb (content) VALUES (?)', stdin)
+  db.close()
+}
+
+// lオプション
+function displayLoption () {
+  db.each('SELECT * FROM foodb', (error, row) => {
+    if (error) {
+      console.error('Error!', error)
+      return
+    }
+    console.log(row.content.split('\n')[0])
+    console.log(row)
+  })
+  db.close()
+}
+
+// dオプション
+function displayDoption () {
+  db.all('SELECT * FROM foodb', (error, rows) => {
+    if (error) {
+      console.error('Error!', error)
+      return
+    }
+    const memoTitles = rows.map(({ content }) => content.split('\n')[0])
+    const memos = rows
+    const values = {
+      type: 'select',
+      name: 'memoTitle',
+      message: 'Choose a note you want to see:',
+      choices: memoTitles,
+    }
+    const answer = Enquirer.prompt(values)
+    answer.then(({ memoTitle }) => {
+      const memoIds = memos.find(element => element.content.split('\n')[0] === memoTitle)
+      const memoId = memoIds.id
+      console.log(memoId)
+      db.run('DELETE FROM foodb WHERE id = ?', memoId, error => {
+        if (error) {
+          return console.error(error.message)
+        }
+      })
+      db.close()
+    })
+  })
+}
+
+// rオプション
+function displayRoption () {
+  db.all('SELECT * FROM foodb', (error, rows) => {
+    if (error) {
+      console.error('Error!', error)
+      return
+    }
+    const memoTitles = rows.map(({ content }) => content.split('\n')[0])
+    const memoAllData = rows.map(({ content }) => content)
+    const values = {
+      type: 'select',
+      name: 'memoTitle',
+      message: 'Choose a note you want to see:',
+      choices: memoTitles,
+    }
+    const answer = Enquirer.prompt(values)
+    answer.then(({ memoTitle }) => {
+      const memoContent = memoAllData.find(element => element.split('\n')[0] === memoTitle)
+      console.log(memoTitle)
+      console.log(memoContent)
+    })
+  })
+  db.close()
+}
+
+// オプション受け付け
+const args = process.argv.slice(2)
+if (args == '-l') {
+  displayLoption()
+} else if (args == '-r') {
+  displayRoption()
+} else if (args == '-d') {
+  displayDoption()
+} else if (args == '') {
+  main(require('fs').readFileSync('/dev/stdin', 'utf8'))
+} else {
+  console.log('引数に誤りがあります')
+}
+
